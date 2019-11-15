58f7c12157043217eebf6177e8186b660b319646
maksdk <havryshchenko@gmail.com>
2019-11-15 08:09:03 +0200

    test for null

--- 07/__tests__/getUserMainLanguage.js
+++ 07/__tests__/getUserMainLanguage.js
@@ -4,10 +4,22 @@ import getFunction from '../functions';
 
 const getUserMainLanguage = getFunction();
 
-const data = [{language: "js"}, {language: "python"}, {language: "js"}];
+const data = [{
+  language: "js"
+}, {
+  language: "python"
+}, {
+  language: "js"
+}];
 
 test("getUserMainLanguage", async () => {
   const lang = await getUserMainLanguage("name user", new OctokitFake(data));
-  
+
   expect(lang).toEqual("js");
+});
+
+test('getUserMainLanguage when empty', async () => {
+  const client = new OctokitFake([]);
+  const mainLanguage = await getUserMainLanguage('user-without-repos', client);
+  expect(mainLanguage).toBeNull();
 });
